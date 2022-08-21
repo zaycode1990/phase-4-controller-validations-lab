25c9f83a744d44d2f44605f19e8fbe6def0dd632
Isaiah <ijandicos90@gmail.com>
2022-08-21 05:28:51 -0400

    Completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,16 +2,16 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
-
+  
   private
   
   def author_params
